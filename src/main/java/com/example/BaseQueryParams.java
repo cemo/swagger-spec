3365dba447bf6e8d44376c64d7b7117a89404920
Cemo <cemalettin.koc@gmail.com>
2021-09-24 23:52:59 +0300

    Clean annotations

--- src/main/java/com/example/BaseQueryParams.java
+++ src/main/java/com/example/BaseQueryParams.java
@@ -1,14 +1,11 @@
 package com.example;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Optional;
 
 public interface BaseQueryParams {
 
-  @JsonProperty("location_id")
   Optional<String> getLocationId();
 
-  @JsonProperty("product_id")
   Optional<Integer> getProductId();
 
 }
--- src/main/java/com/example/ProdcatProductsResponse.java
+++ src/main/java/com/example/ProdcatProductsResponse.java
@@ -1,12 +1,9 @@
 package com.example;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Optional;
 
 public interface ProdcatProductsResponse {
 
-  @JsonProperty("search_meta")
   Optional<ProdcatSearchMetadata> getSearchMetadata();
-
 
 }
