527703a11a64bb0d3dbeac77201d0916a0e99ccf
Cemo <cemalettin.koc@gmail.com>
2021-09-24 23:54:37 +0300

    Clean annotations

--- src/main/java/com/example/BaseQueryParams.java
+++ src/main/java/com/example/BaseQueryParams.java
@@ -1,14 +1,11 @@
 package com.example;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Optional;
 
 public class BaseQueryParams {
 
-  @JsonProperty("location_id")
   public Optional<String> getLocationId(){return null;}
 
-  @JsonProperty("product_id")
   public Optional<Integer> getProductId(){return null;}
 
 }
--- src/main/java/com/example/ProdcatSearchMetadata.java
+++ src/main/java/com/example/ProdcatSearchMetadata.java
@@ -1,31 +1,15 @@
 package com.example;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import java.util.Optional;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import org.immutables.value.Value;
-
 public class ProdcatSearchMetadata extends BaseQueryParams {
 
-  @JsonProperty("page")
   public Page getPage(){
-    return null;
-  }
-
-  @JsonProperty("location_id")
-  public Optional<String> getLocationId(){
     return null;
   }
 
   interface Page {
 
-    @JsonProperty("total_pages")
     int getTotalPages();
 
-    @JsonProperty("total_results")
     int getTotalResults();
   }
 }
